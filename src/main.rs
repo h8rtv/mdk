217dc11b13def6ce5763cbfc4d752f26b532f7cb
Heitor Ventura <heitorventura21@gmail.com>
2023-06-27 16:48:45 -0300

    add basic config and profiles

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::{io::stdout, io::Read, io::Write, time::Duration};
 
 use anyhow::{anyhow, Result};
 use bollard::container::{Config, CreateContainerOptions, StartContainerOptions};
 use bollard::exec::{CreateExecOptions, StartExecResults};
-use bollard::service::ContainerCreateResponse;
-use bollard::Docker;
+use bollard::image::CreateImageOptions;
+use bollard::service::{ContainerCreateResponse, CreateImageInfo, HostConfig};
+use bollard::{Docker, API_DEFAULT_VERSION};
 use clap::{Parser, Subcommand};
-use futures_util::StreamExt;
+use futures_util::{StreamExt, TryStreamExt};
 use termion::async_stdin;
 use termion::raw::IntoRawMode;
 use tokio::io::AsyncWriteExt;
-use tokio::spawn;
 use tokio::time::sleep;
+use tokio::{fs, spawn};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -22,23 +25,69 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    New { name: String },
+    New { profile: String, name: String },
     Connect { name: String },
+    Cfg,
+}
+#[derive(Serialize, Deserialize)]
+struct Profile {
+    image: String,
+    gpu: bool,
+    // ...
+}
+
+#[derive(Serialize, Deserialize)]
+struct MdkConfig {
+    profiles: HashMap<String, Profile>,
+    hostname: String,
+}
+
+impl std::default::Default for MdkConfig {
+    fn default() -> Self {
+        let mut profiles = HashMap::new();
+        profiles.insert(
+            "ubuntu".into(),
+            Profile {
+                image: "ubuntu:latest".into(),
+                gpu: false,
+            },
+        );
+        Self {
+            profiles,
+            hostname: "localhost:2375".into(),
+        }
+    }
 }
 
 async fn create_container(
     docker: &Docker,
     container_name: &str,
-    image_name: &str,
+    profile: &Profile,
 ) -> Result<ContainerCreateResponse> {
     let options = Some(CreateContainerOptions {
         name: container_name,
         platform: None,
     });
 
+    let _: Vec<CreateImageInfo> = docker
+        .create_image(
+            Some(CreateImageOptions {
+                from_image: profile.image.to_owned(),
+                ..Default::default()
+            }),
+            None,
+            None,
+        )
+        .try_collect()
+        .await?;
+
     let config = Config {
-        image: Some(image_name),
+        image: Some(profile.image.to_owned()),
         tty: Some(true),
+        host_config: Some(HostConfig {
+            runtime: Some("nvidia".into()),
+            ..Default::default()
+        }),
         ..Default::default()
     };
 
@@ -92,16 +141,44 @@ async fn connect(docker: &Docker, container_name: &str) -> Result<()> {
     }
 }
 
+async fn get_or_create_config() -> Result<MdkConfig> {
+    match home::home_dir() {
+        Some(homepath) => {
+            let filepath = homepath.join(".mdk");
+            if filepath.exists() {
+                let file = fs::read_to_string(filepath).await?;
+                Ok(serde_yaml::from_str(&file)?)
+            } else {
+                let cfg = MdkConfig {
+                    ..Default::default()
+                };
+                let cfg_str = serde_yaml::to_string(&cfg)?;
+                fs::write(filepath, cfg_str).await?;
+                Ok(cfg)
+            }
+        }
+        None => Err(anyhow!("Couldn't find the home directory")),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let docker = Docker::connect_with_http_defaults()?;
+    let cfg = get_or_create_config().await?;
+    let addr = format!("http://{}", cfg.hostname);
+    let docker = Docker::connect_with_http(&addr, 4, API_DEFAULT_VERSION)?;
     let cli = Cli::parse();
     match &cli.command {
         Commands::Connect { name } => {
             connect(&docker, name).await?;
         }
-        Commands::New { name } => {
-            create_container(&docker, name, "ubuntu:latest").await?;
+        Commands::New { profile, name } => {
+            match cfg.profiles.get(profile) {
+                Some(profile) => Ok(create_container(&docker, name, profile).await?),
+                None => Err(anyhow!("The profile does not exist")),
+            }?;
+        }
+        Commands::Cfg => {
+            println!("{}", serde_yaml::to_string(&cfg)?);
         }
     };
     Ok(())
